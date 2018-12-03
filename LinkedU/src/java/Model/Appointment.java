90323715aadc828e1944590a16831805dd88230d
mlisows <mlisows@WebDev-17.ad.ilstu.edu>
2018-12-03 15:09:59 -0600

    Merge origin/master
    Conflicts:
    	LinkedU/nbproject/project.properties
    	LinkedU/src/java/DAO/projectSQL.sql

---
+++ LinkedU/src/java/Model/Appointment.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author mlisows
+ */
+public class Appointment {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Date date1;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDate1() {
+        return date1;
+    }
+
+    public void setDate1(Date date1) {
+        this.date1 = date1;
+    }
+}
